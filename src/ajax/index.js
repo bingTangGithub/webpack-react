602f1737d1561a8f838e9104062153e4e3e85ddd
wangxueli <1569025161@qq.com>
2017-05-31 14:59:50 +0800

    将 eslint 这个 preLoader 加入 webpack.config.js文件

---
+++ src/ajax/index.js
@@ -0,0 +1,38 @@
+import $ from 'jquery';
+
+$(function(){
+	$("#send").click(function(){
+		console.log('点击');
+		$('#resText').load('./$load.html');
+	})
+
+
+
+
+
+	// $("#ajaxSend").click(function(){
+	// 	let xmlHttpReq = null;
+
+	// 	if(window.ActiveXObject) {  // IE5 IE6浏览器引入 XMLHttpRequest
+	// 		xmlHttpReq = new ActiveXObject("Microsoft.XMLHTTP");
+	// 	} else if(window.XMLHttpRequest) {
+	// 		xmlHttpReq = new XMLHttpRequest(); 
+	// 	}
+
+
+	// 	// xmlHttpReq.open('GET', "http://172.16.1.181:8059/", true);  //调用 open 方法并采用异步方式
+	// 	xmlHttpReq.open('post', "http://106.14.181.21:8008/shop/query", true);  //调用 open 方法并采用异步方式
+	// 	xmlHttpReq.onreadystatechange = RequestCallBack; //设置回调函数  
+	// 	xmlHttpReq.send(null);
+
+	// 	function RequestCallBack() { //一旦readyState值改变，就会调用该函数
+	// 		if(xmlHttpReq.readyState == 4) {
+	// 			if(xmlHttpReq.statue == 200) {
+	// 				document.getElementById("resText").innerHTML =xmlHttpReq.responseText;
+	// 			}
+	// 		}
+
+	// 	}
+
+	// })
+})
--- webpack.config.js
+++ webpack.config.js
@@ -8,71 +8,73 @@ module.exports = {
     // plugins: [commonsPlugin],
     //页面入口文件配置
     entry : {
-        // build00 : "./genjs/index.js",
-        // build01 : "./helloWorld/index.js",
-        // build02 : "./styleObj/index.js",
-        // build03 : "./key/index.js",
-        // build04 : "./doubleComponent/index.jsx",
-        // build05 : "./props/index.js",
-        // build06 : "./children/index.js",
-        // build07 : "./propTypes/index.js",
-        // build08 : "./refs/index.js",
-        // build09 : "./getInitialState/index.js",
-        // build10 : "./demo10/index.js",
-        // build11 : "./demo11/index.js",
-        // build12 : "./demo12/index.js",
-        // build13 : "./demo130/index.js",
-        // build14 : "./demo14/index.js",
-        // build15 : "./demo15/index.js",
-        // build16 : "./demo16/index.js",
-        // build17 : "./onClick/index.js",
-        // build18 : "./demo18/index.js",
-        // build19 : "./demo19/index.js",
-        // build20 : "./demo20/index.jsx",
-        // build21 : "./demo21/index.jsx",
-        // build22 : "./demo22/index.jsx",
-        // build23 : "./demo23/index.jsx",
-        // build24 : "./demo24/index.js",
-        // build25 : "./demo25/index.js",
-        // build26 : "./demo26/index.js",
-        // build27 : "./demo27/index.js"
-        // build28 : "./demo28/index.js"
-        // build29 : "./demo29/index.js",
-        // build30 : "./demo30/index.js",
-        // build31 : "./demo31/index.js",
-        // build32 : "./sonUseFa/index.js",
-        // build33 : "./faUseSon/index.js",
-        // build34 : "./faRenderFirst/index.js",
-        // build35 : "./demo35/index.js",
-        // build36 : "./baiduUtil/index.js",
-        // build37 : "./fetch/index.js",
-        build38 : "./antButton/index.js",
-        // build39 : "./antGrid/index.js",
-        // build40 : "./antBreadcrumb/index.js", //面包屑 有错
-        // build41 : "./antPagination/index.js",
-        // build42 : "./functionParameter/index.js", 
-        // build43 : "./antCascader/index.js", //级联
-        // build44 : "./antCalendar/index.js", //日历
-        // build45 : "./antForm/index.js", //0
-        // build46 : "./antTable/index.js",  
-        // build47 : "./reduxHelloWorld/index.js", 
-        // build48 : "./reduxHeart/index.js",  //redux核心思想  
-        // build49 : "./reduxReducer/index.js", 
-        // build50 : "./reduxTodo/index.js",  
-        // build51 : "./reduxPrinciple/index.js",  
-        // build52 : "./reduxStore/index.js",  
-        // build53 : "./noStateFunction/index.js",  
-        // build54 : "./reactNoStateParameter/index.js",  
-        // build55 : "./antFormJiPei/index.js",  
-        // build56 : "./liGen/index.js",  
-        // build57 : "./antFormJiPeiMine/index.jsx",  
-        // build58 : "./antFormJiPeiTable/index.jsx", 
-        // build59 : "./antFormJiPeiMineFormItem/index.jsx", 
-        // build60 : "./antFormSetstate/index.jsx", 
-        // build61 : "./jieGou/index.jsx", 
-        // build62 : "./antTreeSelect/index.jsx",
-        // build63 : "./export/index.jsx", 
-        // build64 : "./genFormItem/index.jsx", 
+        // build00 : "./src/genjs/index.js",
+        // build01 : "./src/helloWorld/index.js",
+        // build02 : "./src/styleObj/index.js",
+        // build03 : "./src/key/index.js",
+        // build04 : "./src/doubleComponent/index.jsx",
+        // build05 : "./src/props/index.js",
+        // build06 : "./src/children/index.js",
+        // build07 : "./src/propTypes/index.js",
+        // build08 : "./src/refs/index.js",
+        // build09 : "./src/getInitialState/index.js",
+        // build10 : "./src/demo10/index.js",
+        // build11 : "./src/demo11/index.js",
+        // build12 : "./src/demo12/index.js",
+        // build13 : "./src/demo130/index.js",
+        // build14 : "./src/demo14/index.js",
+        // build15 : "./src/demo15/index.js",
+        // build16 : "./src/demo16/index.js",
+        // build17 : "./src/onClick/index.js",
+        // build18 : "./src/demo18/index.js",
+        // build19 : "./src/demo19/index.js",
+        // build20 : "./src/demo20/index.jsx",
+        // build21 : "./src/demo21/index.jsx",
+        // build22 : "./src/demo22/index.jsx",
+        // build23 : "./src/demo23/index.jsx",
+        // build24 : "./src/demo24/index.js",
+        // build25 : "./src/demo25/index.js",
+        // build26 : "./src/demo26/index.js",
+        // build27 : "./src/demo27/index.js"
+        // build28 : "./src/demo28/index.js"
+        // build29 : "./src/demo29/index.js",
+        // build30 : "./src/demo30/index.js",
+        // build31 : "./src/demo31/index.js",
+        // build32 : "./src/sonUseFa/index.js",
+        // build33 : "./src/faUseSon/index.js",
+        // build34 : "./src/faRenderFirst/index.js",
+        // build35 : "./src/demo35/index.js",
+        // build36 : "./src/baiduUtil/index.js",
+        // build37 : "./src/fetch/index.js",
+        // build38 : "./src/antButton/index.js",
+        // build39 : "./src/antGrid/index.js",
+        // build40 : "./src/antBreadcrumb/index.js", //面包屑 有错
+        // build41 : "./src/antPagination/index.js",
+        // build42 : "./src/functionParameter/index.js", 
+        // build43 : "./src/antCascader/index.js", //级联
+        // build44 : "./src/antCalendar/index.js", //日历
+        // build45 : "./src/antForm/index.js", //0
+        // build46 : "./src/antTable/index.js",  
+        // build47 : "./src/reduxHelloWorld/index.js", 
+        // build48 : "./src/reduxHeart/index.js",  //redux核心思想  
+        // build49 : "./src/reduxReducer/index.js", 
+        // build50 : "./src/reduxTodo/index.js",  
+        // build51 : "./src/reduxPrinciple/index.js",  
+        // build52 : "./src/reduxStore/index.js",  
+        // build53 : "./src/noStateFunction/index.js",  
+        // build54 : "./src/reactNoStateParameter/index.js",  
+        // build55 : "./src/antFormJiPei/index.js",  
+        // build56 : "./src/liGen/index.js",  
+        // build57 : "./src/antFormJiPeiMine/index.jsx",  
+        // build58 : "./src/antFormJiPeiTable/index.jsx", 
+        // build59 : "./src/antFormJiPeiMineFormItem/index.jsx", 
+        // build60 : "./src/antFormSetstate/index.jsx", 
+        // build61 : "./src/jieGou/index.jsx", 
+        // build62 : "./src/antTreeSelect/index.jsx",
+        // build63 : "./src/export/index.jsx", 
+        // build64 : "./src/genFormItem/index.jsx", 
+        // build65 : "./src/ajax/index.js", 
+        build66 : "./src/ajaxApi/index.js", 
     },
     //入口文件输出配置
     output : {
@@ -81,6 +83,16 @@ module.exports = {
         filename : '[name].js'
     },
     module: {
+        preLoaders: [{
+            test: /\.(js|jsx)$/,
+            //检测文件类型
+            // include: path.join(__dirname, '/src'),
+            include: __dirname + '/src',
+            //检测文件位置
+            loader: 'eslint-loader'
+            //使用eslint-loader
+        }],
+
         //加载器配置
         loaders: [
 
